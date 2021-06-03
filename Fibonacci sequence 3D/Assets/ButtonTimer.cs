ef49b877b64abd60b00a0f7f2fbcd093b877bbe8
spilrgm <Remco.spil@hva.nl>
2021-06-03 22:31:50 +0200

    Add a timer to the button to stop spamm pressing

---
+++ Fibonacci sequence 3D/Assets/ButtonTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTimer : MonoBehaviour
+{
+
+    public GameObject button;
+
+    public void HideButton()
+    {
+        StartCoroutine(ShowHideButton());
+    }
+
+    public IEnumerator ShowHideButton()
+    {
+        button.SetActive(false);
+        yield return new WaitForSeconds(.25f);
+        button.SetActive(true);
+
+    }
+}
