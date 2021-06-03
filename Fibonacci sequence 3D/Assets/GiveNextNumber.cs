fe2ff374d92293401f053c9f0f7254e84ea8e29f
spilrgm <Remco.spil@hva.nl>
2021-06-03 17:28:26 +0200

    Added the Fibonacci sequence

--- Fibonacci sequence 3D/Assets/GiveNextNumber.cs
+++ Fibonacci sequence 3D/Assets/GiveNextNumber.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GiveNextNumber : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private long[] previousNumbers = new long[2];
+    private long currentNumber;
+
+
+    private int firstNumber = 0;
+    private int secondNumber = 1;
+
+    public Text currentNumberText;
+    public Text previousNumbersText;
+
+    private int numberOfClicks;
+
+    public void Start()
     {
-        
+        previousNumbers[0] = firstNumber;
+        previousNumbers[1] = secondNumber;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GiveNextSequence()
     {
-        
+        //first 2 numbers dont have previous numbers to add up
+        if(numberOfClicks < 1)
+        {
+            currentNumberText.text = firstNumber.ToString();
+            numberOfClicks++;
+            return;
+
+        } else if(numberOfClicks < 2)
+        {
+            currentNumberText.text = secondNumber.ToString();
+            numberOfClicks++;
+            return;
+        }
+
+        //change currentnumber with the next number of the sequence
+        currentNumber = previousNumbers[0] + previousNumbers[1];
+
+        //set the text to the corrosponding numbers
+        previousNumbersText.text = previousNumbers[0].ToString() + " , " + previousNumbers[1].ToString();
+        currentNumberText.text = currentNumber.ToString();
+
+        //Change the last 2 numbers that were added.
+        previousNumbers[0] = previousNumbers[1];
+        previousNumbers[1] = currentNumber;
+
+
     }
+
 }
